e8b89b49f7a416483e1c4a0f20608658cfbc1e0c
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-21 16:29:14 +0200

    Update main.tf

--- devops_01-master/terraform/jenkins/main.tf
+++ devops_01-master/terraform/jenkins/main.tf
@@ -6,7 +6,7 @@ variable "name" {
     description = "Name the instance on deploy"
 }
 
-resource "aws_instance" "groupe01_01_jenkins" {
+resource "aws_instance" "groupe01" {
     ami = "ami-00399ec92321828f5"
     instance_type = "t2.micro"
     key_name = "groupe01"
