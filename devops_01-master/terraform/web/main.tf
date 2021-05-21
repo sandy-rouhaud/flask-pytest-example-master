705265622440ac1d02f753bb71c206eb1767ba39
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-21 15:50:44 +0200

    Update main.tf

--- devops_01-master/terraform/web/main.tf
+++ devops_01-master/terraform/web/main.tf
@@ -6,7 +6,7 @@ variable "name" {
     description="Name of the web box at apply"
 }
 
-resource "aws_instance" "do_web_01" {
+resource "aws_instance" "groupe01_01_web" {
     ami = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
     key_name = "devops_01"
