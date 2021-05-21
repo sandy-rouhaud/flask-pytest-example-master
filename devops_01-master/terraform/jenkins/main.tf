ce8bd25c7bed028e0a5b1569d13bf89ce00ff0fd
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-21 16:36:44 +0200

    Update main.tf

--- devops_01-master/terraform/jenkins/main.tf
+++ devops_01-master/terraform/jenkins/main.tf
@@ -9,7 +9,7 @@ variable "name" {
 resource "aws_instance" "groupe01" {
     ami = "ami-00399ec92321828f5"
     instance_type = "t2.micro"
-    key_name = "groupe01"
+    key_name = "gkp-groupe-1-CCSEC"
 
     tags = {
         Name = "${var.name}"
