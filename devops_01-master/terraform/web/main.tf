cb7e929c6f55290f29d12876b15871d9f86d01e6
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-24 12:41:40 +0200

    Update main.tf

--- devops_01-master/terraform/web/main.tf
+++ devops_01-master/terraform/web/main.tf
@@ -6,13 +6,17 @@ variable "name" {
     description="Name of the web box at apply"
 }
 
+resource "aws_eip_association" "eip_assoc" {
+    allocation_id = "eipalloc-09832f47cab8f5691"
+}
+
+
 resource "aws_instance" "groupe01" "aws_eip_association" "eip_assoc" {
     ami = "ami-00399ec92321828f5"
     instance_type = "t2.micro"
     key_name = "kp-groupe-1-CCSEC"
     vpc_security_group_ids = ["sg-0df89b1f79f8cbf01"]
     subnet_id = "subnet-08ac909c951e9226f"
-    allocation_id = "eipalloc-09832f47cab8f5691"
 
     tags = {
         Name = "${var.name}"
