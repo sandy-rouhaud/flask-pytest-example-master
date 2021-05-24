81859806cd41987ab5d01b5c49d97f356331a58e
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-24 11:02:32 +0200

    VPC et subnet
        vpc_security_group_ids = "sg-03d0a94ec75785a93"
        subnet_id = ["subnet-08ac909c951e9226f","subnet-05c9ca18b81dc8721","subnet-0fab122cda4715e50"]

--- devops_01-master/terraform/jenkins/main.tf
+++ devops_01-master/terraform/jenkins/main.tf
@@ -10,6 +10,8 @@ resource "aws_instance" "groupe01" {
     ami = "ami-00399ec92321828f5"
     instance_type = "t2.micro"
     key_name = "kp-groupe-1-CCSEC"
+    vpc_security_group_ids = "sg-03d0a94ec75785a93"
+    subnet_id = ["subnet-08ac909c951e9226f","subnet-05c9ca18b81dc8721","subnet-0fab122cda4715e50"]
 
     tags = {
         Name = "${var.name}"
