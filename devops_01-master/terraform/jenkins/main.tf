f8e78a98d746195fa8ab0f3d825684d76752f16f
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-24 11:17:52 +0200

    Update main.tf

--- devops_01-master/terraform/jenkins/main.tf
+++ devops_01-master/terraform/jenkins/main.tf
@@ -10,8 +10,8 @@ resource "aws_instance" "groupe01" {
     ami = "ami-00399ec92321828f5"
     instance_type = "t2.micro"
     key_name = "kp-groupe-1-CCSEC"
-    vpc_security_group_ids = "sg-03d0a94ec75785a93"
-
+    vpc_security_group_ids = ["sg-03d0a94ec75785a93"]
+ 
     tags = {
         Name = "${var.name}"
     }
