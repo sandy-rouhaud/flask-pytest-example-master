104a249779f091be0bdc1f6ae904f4d186a2a708
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-24 11:11:10 +0200

    Update main.tf

--- devops_01-master/terraform/web/main.tf
+++ devops_01-master/terraform/web/main.tf
@@ -10,6 +10,8 @@ resource "aws_instance" "groupe01" {
     ami = "ami-00399ec92321828f5"
     instance_type = "t2.micro"
     key_name = "kp-groupe-1-CCSEC"
+    vpc_security_group_ids = "sg-0df89b1f79f8cbf01"
+    subnet_id = "subnet-08ac909c951e9226f"
 
     tags = {
         Name = "${var.name}"
