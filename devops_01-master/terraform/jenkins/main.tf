cec09ed209b3125f43814bd78a7ba9fc2580d871
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-24 12:41:06 +0200

    Update main.tf

--- devops_01-master/terraform/jenkins/main.tf
+++ devops_01-master/terraform/jenkins/main.tf
@@ -6,13 +6,17 @@ variable "name" {
     description = "Name the instance on deploy"
 }
 
-resource "aws_instance" "groupe01" "aws_eip_association" "eip_assoc" {
+resource "aws_eip_association" "eip_assoc" {
+    allocation_id = "eipalloc-036ebd6ca5e2b187a"
+}
+
+resource "aws_instance" "groupe01" {
     ami = "ami-00399ec92321828f5"
     instance_type = "t2.micro"
     key_name = "kp-groupe-1-CCSEC"
     vpc_security_group_ids = ["sg-03d0a94ec75785a93"]
     subnet_id = "subnet-08ac909c951e9226f"
-    allocation_id = "eipalloc-036ebd6ca5e2b187a"
+
  
     tags = {
         Name = "${var.name}"
