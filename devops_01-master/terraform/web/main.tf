de5f4f19b4920f9f9318aae298b1a8d908b5d7e2
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-24 11:12:24 +0200

    Update main.tf

--- devops_01-master/terraform/web/main.tf
+++ devops_01-master/terraform/web/main.tf
@@ -11,7 +11,6 @@ resource "aws_instance" "groupe01" {
     instance_type = "t2.micro"
     key_name = "kp-groupe-1-CCSEC"
     vpc_security_group_ids = "sg-0df89b1f79f8cbf01"
-    subnet_id = "subnet-08ac909c951e9226f"
 
     tags = {
         Name = "${var.name}"
