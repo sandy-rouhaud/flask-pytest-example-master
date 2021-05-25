8a91e2825526f01d787aa1ecfc3ebea4277fe662
Sandy <sandy.rouhaud@gmail.com>
2021-05-25 10:53:00 +0200

    jenkins02 main.tf

---
+++ devops_01-master/terraform/jenkins02/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+variable "name" {
+    description = "Name the instance on deploy"
+}
+
+#resource "aws_eip_association" "eip_assoc" {
+#    instance_id   = "i-0b6a36c89863a1cec"
+#    allocation_id = "eipalloc-0ec6889136d977ec8"
+#}
+
+resource "aws_instance" "groupe01" {
+    ami = "ami-00399ec92321828f5"
+    instance_type = "t2.micro"
+    key_name = "kp-groupe-1-CCSEC"
+    vpc_security_group_ids = ["sg-03d0a94ec75785a93"]
+    subnet_id = "subnet-05c9ca18b81dc8721"
+
+ 
+    tags = {
+        Name = "${var.name}"
+    }
+}
