dfb1ff41543f58d18d1ed13ccf24b4fe75db9c25
Sandy <sandy.rouhaud@gmail.com>
2021-05-25 11:21:45 +0200

    web 02

---
+++ devops_01-master/terraform/web02/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region="us-east-2"
+}
+
+variable "name" {
+    description="Name of the web box at apply"
+}
+
+#resource "aws_eip_association" "eip_assoc" {
+#    instance_id   = "i-0d9989848ea433532"
+#    allocation_id = "eipalloc-09832f47cab8f5691"
+#}
+
+
+resource "aws_instance" "groupe01" {
+    ami = "ami-00399ec92321828f5"
+    instance_type = "t2.micro"
+    key_name = "kp-groupe-1-CCSEC"
+    vpc_security_group_ids = ["sg-0df89b1f79f8cbf01"]
+    subnet_id = "subnet-0fab122cda4715e50"
+
+    tags = {
+        Name = "${var.name}"
+    }
+}
