7b0d4748dcd39290428d0baf1539a288d505fe88
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-27 14:09:09 +0200

    Update main.tf

--- devops_01-master/terraform/web04/main.tf
+++ devops_01-master/terraform/web04/main.tf
@@ -13,8 +13,8 @@ resource "aws_eip_association" "eip_assoc" {
 
 
 resource "aws_instance" "groupe01" {
-    ami = "ami-00399ec92321828f5"
-    instance_type = "t2.micro"
+    ami = "ami-077e31c4939f6a2f3"
+    instance_type = "t2.medium"
     key_name = "kp-groupe-1-CCSEC"
     vpc_security_group_ids = ["sg-0df89b1f79f8cbf01"]
     subnet_id = "subnet-08ac909c951e9226f"
